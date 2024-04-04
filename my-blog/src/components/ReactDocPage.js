630044f67eb9b12d2365dab3ecd101c4ca810e4f
okwonye@gmail.com <okwonye@gmail.com>
2024-04-04 22:50:33 +0900

    - ReactDocPage 내에서 개별 포스팅 title / body 내용 추가하기 - 개별 페이지에서 id 값에 해당하는 api 요청 - response로 setData() 를 통해 포스팅 내용 업데이트 - return 문에 DOM 추가

--- my-blog/src/components/ReactDocPage.js
+++ my-blog/src/components/ReactDocPage.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function ReactDocPage() {
   const params = useParams();
+  const [data, setData] = useState({});
 
-  return <div>ReactDocPage ##{params.docId}</div>;
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${params.docId}`
+      );
+      const result = await res.json();
+      return result;
+    }
+
+    fetchData().then((res) => {
+      setData(res);
+    });
+  }, []);
+
+  return (
+    <>
+      <div>ReactDocPage ##{params.docId}</div>
+      <h3>{data.title}</h3>
+      <div>{data.body}</div>
+    </>
+  );
 }
 
 export default ReactDocPage;
