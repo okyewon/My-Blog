fabb13650c9f77f81071fbf67e94b63423e27825
okwonye@gmail.com <okwonye@gmail.com>
2024-04-04 23:52:42 +0900

    - useSWR 사용해보기 - useSWR은 useEffect() 안에서 사용 불가 - 최초 mount 될 때만 api를 요청하도록 작동하기 때문에 useEffect 불필요 - 캐시에 저장해두기 때문에 rerendering 시 api를 재호출하지 않음

--- my-blog/src/components/ReactPage.js
+++ my-blog/src/components/ReactPage.js
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import axios from "axios";
+import useSWR from "swr";
 
 function ReactPage() {
-  const [docs, setDocs] = useState([]);
+  // const [docs, setDocs] = useState([]);
+  const [number, setNumber] = useState(0);
 
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      // const result = await res.json();
-      console.log(result);
-      return result.data;
-    }
+  async function fetcher() {
+    const result = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
 
-    fetchData().then((res) => {
-      setDocs(res);
-    });
-  }, []);
+    console.log(result.data);
+    return result.data;
+  }
+  const { data: docs, error } = useSWR("posts", fetcher);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const result = await axios.get(
+  //       "https://jsonplaceholder.typicode.com/posts"
+  //     );
+  //     // const result = await res.json();
+  //     console.log(result);
+  //     return result.data;
+  //   }
+
+  //   fetchData().then((res) => {
+  //     setDocs(res);
+  //   });
+  // }, []);
+  if (error) return <div>failed to load</div>;
+  if (!docs) return <div>loading...</div>;
 
   return (
     <div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
       {docs.map((doc) => (
         <Link
           to={`${doc.id}`}
