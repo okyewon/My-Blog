f278c38dc71ed397839dfe1435c80d356f647e4b
okwonye@gmail.com <okwonye@gmail.com>
2024-04-05 15:11:29 +0900

    - index.js의 React.StrictMode 로 console.log가 두 번씩 찍혀서 수정 - useReducer 사용을 위해 blog 페이지에서 user.js 컴포넌트 내에 생성한 useReducer 상태 값을 변환 - useReducer 를 Context API 와 함께 사용해서 전역 상태 관리가 가능하도록 구현

--- my-blog/src/components/BlogPage.js
+++ my-blog/src/components/BlogPage.js
@@ -2,11 +2,17 @@ import React, { useContext } from "react";
 import { UserContext } from "../store/user.js";
 
 function BlogPage() {
-  const value = useContext(UserContext);
-  console.log(value);
+  const dispatch = useContext(UserContext);
+  console.log(dispatch);
+
   return (
     <div>
       <h1>BlogPage</h1>
+      <button
+        onClick={() => dispatch({ type: "changeJob", text: "BE-developer" })}
+      >
+        Change Job
+      </button>
     </div>
   );
 }
--- my-blog/src/store/user.js
+++ my-blog/src/store/user.js
@@ -1,17 +1,38 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer, useState } from "react";
 
 export const UserContext = createContext();
 
+const initialUser = {
+  name: "ogi",
+  job: "FE-developer",
+};
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case "changeJob":
+      return { ...state, job: action.text };
+      break;
+
+    default:
+      break;
+  }
+};
+
 function UserStore(props) {
-  const [job, setJob] = useState("FE-developer");
-  const user = {
-    name: "ogi",
-    job: "FE-developer",
-    changeJob: (updatedJob) => setJob(updatedJob),
-  };
+  // const [job, setJob] = useState("FE-developer");
+  const [user, dispatch] = useReducer(userReducer, initialUser);
+
+  // const user = {
+  // name: "ogi",
+  // job: "FE-developer",
+  // changeJob: (updatedJob) => setJob(updatedJob),
+  // };
+
+  console.log(user);
 
   return (
-    <UserContext.Provider value={user}>{props.children}</UserContext.Provider>
+    <UserContext.Provider value={dispatch}>
+      {props.children}
+    </UserContext.Provider>
   );
 }
 
